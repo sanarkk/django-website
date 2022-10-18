6a9fbe0c60042f030e7f4aaf0ec54cedd7857b80
sanarkk <sanya.dimbrovskyi@gmail.com>
2022-10-18 12:45:38 -0500

    Developed phone number validation and error messages. Bug with login form messages.

--- djangoauth/forms.py
+++ djangoauth/forms.py
@@ -2,6 +2,7 @@ from allauth.account.forms import SignupForm
 from django.utils.translation import gettext_lazy
 from django import forms
 from .models import UserProfile
+from django.core.validators import RegexValidator
 
 
 class CustomSignupForm(SignupForm):
@@ -18,8 +19,13 @@ class CustomSignupForm(SignupForm):
         required=True,
     )
     phone_number = forms.CharField(
-        max_length=40,
-        required=True,
+        max_length=15,
+        validators=[
+            RegexValidator(
+                r'^\+?1?\d{9,15}$',
+                message="Enter a Valid Phone Number",
+            )
+        ],
     )
     password1 = forms.CharField(
         max_length=40,
--- djangoauth/models.py
+++ djangoauth/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import RegexValidator
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -9,9 +10,10 @@ class UserProfile(models.Model):
     )
     first_name = models.CharField(max_length=40)
     last_name = models.CharField(max_length=40)
-    phone_number = models.CharField(max_length=18)
+
+    phone_number = models.CharField(max_length=15)
 
     objects = models.Manager()
 
     def __str__(self):
-        return f'{self.first_name} | {self.last_name}'
+        return f"{self.first_name} | {self.last_name}"
