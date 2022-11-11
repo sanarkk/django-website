a03bc883d264d834a597f59476c691db27183065
sanarkk <sanya.dimbrovskyi@gmail.com>
2022-11-11 10:29:37 -0600

    Bug with prodile update.

--- user_profile/forms.py
+++ user_profile/forms.py
@@ -4,6 +4,23 @@ from django.contrib.auth.models import User
 
 
 class UpdateProfileForm(forms.ModelForm):
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+
     class Meta:
         model = UserProfile
-        fields = ['avatar', 'phone_number']
+        fields = ["avatar", "phone_number"]
+
+    def __init__(self, request, *args, **kwargs):
+        self.request = request
+        super(UpdateProfileForm, self).__init__(*args, **kwargs)
+
+    #def save(self, request):
+    #    user = super().save(request)
+    #    first_name = self.cleaned_data["first_name"]
+    #    last_name = self.cleaned_data["last_name"]
+    #    p, _ = User.objects.get_or_create(
+    #        user=user, first_name=first_name, last_name=last_name
+    #    )
+    #    return user
+
--- user_profile/views.py
+++ user_profile/views.py
@@ -2,7 +2,7 @@ from braces.views import LoginRequiredMixin
 from django.views.generic import TemplateView
 from djangoauth.models import UserProfile
 from django.views.generic.edit import UpdateView
-from .forms import UpdateProfileForm# Create your views here
+from .forms import UpdateProfileForm  # Create your views here
 from django.contrib.auth.models import User
 
 
@@ -18,14 +18,33 @@ class ProfileView(LoginRequiredMixin, TemplateView):
 
 class UpdateProfile(LoginRequiredMixin, UpdateView):
     model = UserProfile
-    template_name = 'user_profile/update_profile.html'
+    template_name = "user_profile/update_profile.html"
     form_class = UpdateProfileForm
 
-    #fields = [
+    def get_form_kwargs(self):
+        kwargs = super(UpdateProfile, self).get_form_kwargs()
+        kwargs.update({
+            'request': self.request.user,
+        })
+        return kwargs
+
+    def form_valid(self, form):
+        form_up = UpdateProfileForm
+        if form_up.is_valid(form):
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            user = User()
+            user.first_name = first_name
+            user.last_name = last_name
+            user.save()
+            return user
+
+
+    # fields = [
     #    'first_name',
     #    'last_name',
     #    #'avatar',
     #    #'phone_number',
-    #]
+    # ]
 
-    success_url = '/'
+    success_url = "/"
