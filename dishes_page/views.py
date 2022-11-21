9f2cdd181bec9531d0efe2bc462162119ac30468
sanarkk <sanya.dimbrovskyi@gmail.com>
2022-11-21 13:24:42 -0600

    Developed routing for dishes page.

--- dishes_page/views.py
+++ dishes_page/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from braces.views import LoginRequiredMixin
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+# Create your views here
+
+
+class DishesView(LoginRequiredMixin, TemplateView):
+    template_name = "dishes_page/dishes.html"
+
+    login_url = "/"
+    raise_exception = True
+
+    def get(self, request, **kwargs):
+        return self.render_to_response({})
