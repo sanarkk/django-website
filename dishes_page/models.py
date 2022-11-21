08350c5bab0d57cd4f81f9f98e6406815ac3371d
sanarkk <sanya.dimbrovskyi@gmail.com>
2022-11-21 13:17:39 -0600

    Developed model for dishes

---
+++ dishes_page/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django_resized import ResizedImageField
+
+
+# Create your models here.
+class Dish(models.Model):
+    name = models.CharField(max_length=40)
+    description = models.TextField()
+    ingredients = models.TextField()
+    serving_size = models.IntegerField()
+    price = models.IntegerField()
+    image = ResizedImageField(
+        size=[150, 150],
+        upload_to="dishes",
+        blank=True,
+        null=True,
+    )
+
+    objects = models.Manager()
+
+    def __str__(self):
+        return f"{self.name}"
